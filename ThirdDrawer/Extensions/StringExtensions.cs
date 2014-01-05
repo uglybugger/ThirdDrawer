4e4a11e7a745da056c7327dc93daa5ae7c2fdfd9
uglybugger <andrewh@uglybugger.org>
2014-01-05 22:25:47 +1000

    Imported a small handful of common extension methods.

---
+++ ThirdDrawer/Extensions/StringExtensions.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ThirdDrawer.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string FormatWith(this string s, params object[] args)
+        {
+            return string.Format(s, args);
+        }
+
+        public static string GenerateRandomString(int length)
+        {
+            var characters = new List<char>();
+
+            var random = new Random();
+            for (var i = 0; i < length; i++)
+            {
+                var asciiCode = random.Next(33, 126); // ! to ~ in ASCII
+                var c = Convert.ToChar(asciiCode);
+                characters.Add(c);
+            }
+
+            return new string(characters.ToArray());
+        }
+
+        public static string HashWithSalt(this string stringToHash, string salt)
+        {
+            var encoding = new UTF8Encoding();
+            var saltBytes = encoding.GetBytes(salt);
+            var hashBytes = encoding.GetBytes(stringToHash);
+
+            for (var i = 0; i < 50; i++)
+            {
+                var saltedBytesToHash = hashBytes.ToList();
+                saltedBytesToHash.AddRange(saltBytes);
+
+                using (var hash = new SHA512Managed())
+                {
+                    hashBytes = hash.ComputeHash(saltedBytesToHash.ToArray());
+                }
+            }
+
+            return Convert.ToBase64String(hashBytes);
+        }
+    }
+}
---
+++ ThirdDrawer/Extensions/TypeExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ThirdDrawer.Extensions
+{
+    public static class TypeExtensions
+    {
+        public static bool IsInstantiable(this Type type)
+        {
+            if (type.IsInterface) return false;
+            if (type.IsAbstract) return false;
+            if (type.ContainsGenericParameters) return false;
+            return true;
+        }
+    }
+}
