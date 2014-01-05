f25609e73f2d403f84089642c1cf530abefefdd1
uglybugger <andrewh@uglybugger.org>
2014-01-05 22:16:16 +1000

    Added unit test.

---
+++ ThirdDrawer.UnitTests/WhenTheTestHarnessRuns.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace ThirdDrawer.UnitTests
+{
+    [TestFixture]
+    public class WhenTheTestHarnessRuns
+    {
+        [Test]
+        public void ThePeopleShallRejoice()
+        {
+        }
+    }
+}
