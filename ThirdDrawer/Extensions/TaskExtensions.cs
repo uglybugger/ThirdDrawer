9430733d53d4cf21f2b3b806cf359f57957bc685
uglybugger <andrewh@uglybugger.org>
2014-01-07 14:48:41 +1000

    Added Task.WaitForResult extension method.

---
+++ ThirdDrawer/Extensions/TaskExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ThirdDrawer.Extensions
+{
+    public static class TaskExtensions
+    {
+        public static TResult WaitForResult<TResult>(this Task<TResult> task)
+        {
+            task.Wait();
+            return task.Result;
+        }
+
+        public static TResult WaitForResult<TResult>(this Task<TResult> task, TimeSpan timeout)
+        {
+            task.Wait(timeout);
+            return task.Result;
+        }
+    }
+}
