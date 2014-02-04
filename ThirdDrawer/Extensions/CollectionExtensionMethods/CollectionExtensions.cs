df992a69cee6c81bcd8f6046007361074eebf963
uglybugger <andrewh@uglybugger.org>
2014-02-04 12:29:01 +1000

    Moved classes into separate namespaces to reduce the likelihood of namespace collisions with other projects.

---
+++ ThirdDrawer/Extensions/CollectionExtensionMethods/CollectionExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ThirdDrawer.Extensions.CollectionExtensionMethods
+{
+    public static class CollectionExtensions
+    {
+        public static bool None<T>(this IEnumerable<T> items)
+        {
+            return !items.Any();
+        }
+
+        public static bool None<T>(this IEnumerable<T> items, Func<T, bool> predicate)
+        {
+            return !items.Any(predicate);
+        }
+
+        public static IEnumerable<T> NotNull<T>(this IEnumerable<T> items) where T : class
+        {
+            return items.Where(item => item != null);
+        }
+
+        public static IEnumerable<T> Do<T>(this IEnumerable<T> items, Action<T> action)
+        {
+            foreach (var item in items)
+            {
+                action(item);
+                yield return item;
+            }
+        }
+
+        public static void Done<T>(this IEnumerable<T> items)
+        {
+            var enumerator = items.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                // just force enumeration; that's all.
+            }
+        }
+    }
+}
---
+++ ThirdDrawer/Extensions/TypeExtensionMethods/TypeExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ThirdDrawer.Extensions.TypeExtensionMethods
+{
+    public static class TypeExtensions
+    {
+        public static bool IsInstantiable(this Type type)
+        {
+            if (type.IsInterface) return false;
+            if (type.IsAbstract) return false;
+            if (type.ContainsGenericParameters) return false;
+            return true;
+        }
+
+        public static bool IsAssignableTo<TTarget>(this Type type)
+        {
+            return typeof (TTarget).IsAssignableFrom(type);
+        }
+
+        /// <summary>
+        ///     Alias for IsAssignableTo.
+        /// </summary>
+        /// <remarks>
+        ///     This alias is to avoid collisions with Autofac's extension method of the same name.
+        /// </remarks>
+        public static bool CanBeAssignedTo<TTarget>(this Type type)
+        {
+            return IsAssignableTo<TTarget>(type);
+        }
+    }
+}
