fdef0bc150ca6126bf01d102a76ccd32eb269440
Andrew Harcourt <andrewh@uglybugger.org>
2015-10-29 18:07:55 +1000

    Added check for IsClosedTypeOf.

---
+++ src/ThirdDrawer.UnitTests/TypeExtensionTests/WhenLookingForClosedTypesOfAnOpenGeneric.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Shouldly;
+using ThirdDrawer.Extensions.TypeExtensionMethods;
+
+namespace ThirdDrawer.UnitTests.TypeExtensionTests
+{
+    [TestFixture]
+    public class WhenLookingForClosedTypesOfAnOpenGeneric
+    {
+        private class TestCases : IEnumerable<TestCaseData>
+        {
+            public IEnumerator<TestCaseData> GetEnumerator()
+            {
+                yield return new TestCaseData(typeof (ISomeGenericInterface<>), typeof (ISomeGenericInterface<>), false);
+                yield return new TestCaseData(typeof (ISomeGenericInterface<int>), typeof (ISomeGenericInterface<>), true);
+                yield return new TestCaseData(typeof (ISomeGenericInterface<string>), typeof (ISomeGenericInterface<>), true);
+                yield return new TestCaseData(typeof (SomeClass), typeof (ISomeGenericInterface<>), true);
+                yield return new TestCaseData(typeof (SomeDerivedClass), typeof (ISomeGenericInterface<>), true);
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return GetEnumerator();
+            }
+        }
+
+        public class SomeDerivedClass : SomeClass
+        {
+        }
+
+        public class SomeClass : ISomeGenericInterface<int>
+        {
+        }
+
+        public interface ISomeGenericInterface<T>
+        {
+        }
+
+        [Test]
+        [TestCaseSource(typeof (TestCases))]
+        public void WeShouldGetTheRightAnswer(Type candidateType, Type openGenericType, bool expectedResult)
+        {
+            candidateType.IsClosedTypeOf(openGenericType).ShouldBe(expectedResult);
+        }
+    }
+}
--- src/ThirdDrawer/Extensions/TypeExtensionMethods/TypeExtensions.cs
+++ src/ThirdDrawer/Extensions/TypeExtensionMethods/TypeExtensions.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using ThirdDrawer.Extensions.CollectionExtensionMethods;
 
 namespace ThirdDrawer.Extensions.TypeExtensionMethods
 {
@@ -26,6 +28,33 @@ namespace ThirdDrawer.Extensions.TypeExtensionMethods
         public static bool CanBeAssignedTo<TTarget>(this Type type)
         {
             return IsAssignableTo<TTarget>(type);
+        }
+
+        public static bool IsClosedTypeOf(this Type type, Type openGenericType)
+        {
+            if (!openGenericType.IsGenericType) throw new ArgumentException("It's a bit difficult to have a closed type of a non-open-generic type", "openGenericType");
+
+            var interfaces = type.GetInterfaces();
+            var baseTypes = new[] {type}.DepthFirst(t => t.BaseType == null ? new Type[0] : new[] {t.BaseType});
+            var typeAndAllThatThatEntails = new[] {type}.Union(interfaces).Union(baseTypes).ToArray();
+            var genericTypes = typeAndAllThatThatEntails.Where(i => i.IsGenericType);
+            var closedGenericTypes = genericTypes.Where(i => !i.IsGenericTypeDefinition);
+            var assignableGenericTypes = closedGenericTypes.Where(i => openGenericType.IsAssignableFrom(i.GetGenericTypeDefinition()));
+
+            return assignableGenericTypes.Any();
+        }
+
+        public static bool IsClosedTypeOf(this Type type, params Type[] openGenericTypes)
+        {
+            return openGenericTypes.Any(type.IsClosedTypeOf);
+        }
+
+        public static Type[] GetGenericInterfacesClosing(this Type type, Type genericInterface)
+        {
+            var genericInterfaces = type.GetInterfaces()
+                .Where(i => i.IsClosedTypeOf(genericInterface))
+                .ToArray();
+            return genericInterfaces;
         }
     }
 }
