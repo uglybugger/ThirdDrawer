4ce1b7d6621e25538e54e5e5db238a61e174609e
mgiuffri <marianogiuffrida@hotmail.it>
2016-08-02 22:46:02 +1000

    Added function to partition an Ienumerable based on a given predicate.

---
+++ src/ThirdDrawer/Extensions/CollectionExtensionMethods/Partition.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace ThirdDrawer.Extensions.CollectionExtensionMethods
+{
+    public sealed class Partition<T>
+    {
+        public Partition(IEnumerable<T> satisfies, IEnumerable<T> notsatisfies)
+        {
+            Satisfies = satisfies;
+            DoesNotSatisfy = notsatisfies;
+        }
+
+        public IEnumerable<T> Satisfies { get; }
+
+        public IEnumerable<T> DoesNotSatisfy { get; }
+    }
+}
