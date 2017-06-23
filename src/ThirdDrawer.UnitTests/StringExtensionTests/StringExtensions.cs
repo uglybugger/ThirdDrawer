481e6c81863e08e55356359f5ceb46fef6ada93f
DOMINOS\Javid.Najafi <javid.najafi@dominos.com.au>
2017-06-23 15:25:05 +1000

    Added StringExtensions Unit Tests (NOT covering all methods and all scenarios)

---
+++ src/ThirdDrawer.UnitTests/StringExtensionTests/StringExtensions.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Text;
+using NUnit.Framework;
+using Shouldly;
+using ThirdDrawer.Extensions.StringExtensionMethods;
+
+namespace ThirdDrawer.UnitTests.StringExtensionTests
+{
+    [TestFixture]
+    public class StringExtensions
+    {
+        [Test]
+        public void TheResultShouldBeCorrect()
+        {
+            var testcase1Result = "foo".EqualCaseInsensitive("foO");
+            testcase1Result.ShouldBe(true);
+
+            var testcase2Result = "Foo".EqualCaseInsensitive("foO");
+            testcase2Result.ShouldBe(true);
+
+            var testcase3Result = "Fo0".EqualCaseInsensitive("foO");
+            testcase3Result.ShouldBe(false);
+
+        }
+
+        [Test]
+        public void EqualCaseInsensitiveShouldReturnTrueWhenPassingEmptryString()
+        {
+            var testcase1Result = string.Empty.EqualCaseInsensitive("");
+            testcase1Result.ShouldBe(true);
+        }
+
+        [Test]
+        public void HasValShouldReturnFalseWhenPassingEmptryString()
+        {
+            var testcase1Result = string.Empty.HasVal();
+            testcase1Result.ShouldBe(false);
+        }
+
+        [Test]
+        public void HasValShouldReturnFalseWhenPassingNull()
+        {
+            string param = null;
+            var testcase1Result = param.HasVal();
+            testcase1Result.ShouldBe(false);
+        }
+
+        [Test]
+        public void NoValShouldReturnTrueWhenPassingEmptryString()
+        {
+            var testcase1Result = string.Empty.NoVal();
+            testcase1Result.ShouldBe(true);
+        }
+
+        [Test]
+        public void NoValShouldReturnTrueWhenPassingNull()
+        {
+            string param = null;
+            var testcase1Result = param.NoVal();
+            testcase1Result.ShouldBe(true);
+        }
+
+    }
+}
